ca24c04881d70fd32940c70556857a3b84959395
timp <thonghitak.tim@gmail.com>
2022-06-20 15:56:54 +0700

    feat : setup modular project.

---
+++ app/src/main/java/com/mangofactory/androidsimpleapp/SimpleApplication.kt
@@ -0,0 +1,5 @@
+package com.mangofactory.androidsimpleapp
+
+import android.app.Application
+
+class SimpleApplication : Application()
---
+++ buildSrc/src/main/java/com/mangofactory/buildsrc/MyClass.kt
@@ -0,0 +1,4 @@
+package com.mangofactory.buildsrc
+
+class MyClass {
+}
